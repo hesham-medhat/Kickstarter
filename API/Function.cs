52811052b4b60f76748117c5621cc50f6a90a505
hesham-medhat <heshammedhat5@gmail.com>
2020-06-14 20:37:06 +0200

    Implement TestLambdaFunction

--- API/Function.cs
+++ API/Function.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.DynamoDBEvents;
 using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Threading.Tasks;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -13,19 +15,19 @@ namespace API
 {
     public class Function
     {
-        public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+        public void TestLambdaFunction (APIGatewayProxyRequest inputRequest, ILambdaContext context)
         {
-            context.Logger.LogLine($"Beginning to process {dynamoEvent.Records.Count} records...");
+            context.Logger.LogLine($"Beginning to process event: {inputRequest.Path}");
 
-            foreach (var record in dynamoEvent.Records)
+            foreach (var parameterKeyValue in inputRequest.PathParameters)
             {
-                context.Logger.LogLine($"Event ID: {record.EventID}");
-                context.Logger.LogLine($"Event Name: {record.EventName}");
-				
-				// TODO: Add business logic processing the record.Dynamodb object.
+                context.Logger.LogLine($"Path Parameter: {parameterKeyValue.Key}" +
+                    $"-> Value: {parameterKeyValue.Value}");
             }
 
-            context.Logger.LogLine("Stream processing complete.");
+            // TODO: Add business logic processing the request
+
+            context.Logger.LogLine("Processing request complete.");
         }
     }
 }
