10a5be85d61a87c2e96c45fc23adfb42b287d8c4
hesham-medhat <heshammedhat5@gmail.com>
2020-06-15 03:26:06 +0200

    Add to TestLambdaFunction implementation for accessing and examining DynamoDB

--- API/Function.cs
+++ API/Function.cs
@@ -7,6 +7,7 @@ using Amazon.Lambda.DynamoDBEvents;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -15,7 +16,9 @@ namespace API
 {
     public class Function
     {
-        public void TestLambdaFunction (APIGatewayProxyRequest inputRequest, ILambdaContext context)
+        private static AmazonDynamoDBClient _dynamoDBClient = new AmazonDynamoDBClient();
+
+        public async void TestLambdaFunction (APIGatewayProxyRequest inputRequest, ILambdaContext context)
         {
             context.Logger.LogLine($"Beginning to process event: {inputRequest.Path}");
 
@@ -23,6 +26,16 @@ namespace API
             {
                 context.Logger.LogLine($"Path Parameter: {parameterKeyValue.Key}" +
                     $"-> Value: {parameterKeyValue.Value}");
+            }
+
+            /* Access DynamoDB */
+
+            context.Logger.LogLine($"Connected DynamoDB was observed to have the tables:");
+
+            var tablesListing = await _dynamoDBClient.ListTablesAsync();
+            
+            foreach (string tableName in tablesListing.TableNames) {
+                context.Logger.LogLine(tableName);
             }
 
             // TODO: Add business logic processing the request
