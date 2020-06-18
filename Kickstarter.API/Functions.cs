60a42af636ea0938ea983b54ce182b6b0829ea5c
hesham-medhat <heshammedhat5@gmail.com>
2020-06-18 18:18:22 +0200

    Add error handling code for GetPost

--- Kickstarter.API/Functions.cs
+++ Kickstarter.API/Functions.cs
@@ -6,6 +6,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 
 using Kickstarter.Core.Services;
+using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -22,12 +23,26 @@ namespace Kickstarter.API
         public async Task<APIGatewayProxyResponse> GetPost(APIGatewayProxyRequest request, ILambdaContext context)
         {
             string id = request.PathParameters["postid"];
-            var response = new APIGatewayProxyResponse
+
+            var response = new APIGatewayProxyResponse();
+
+            response.Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } };
+
+            try
             {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = await PostsService.GetPostByIdAsync(id),
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
-            };
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Body = await PostsService.GetPostByIdAsync(id);
+            }
+            catch (Exception e)
+            {
+                int statusCode = response.StatusCode = Convert.ToInt32(e.Message);
+                response.Body = statusCode switch
+                {
+                    404 => "Requested post does not exist",
+                    _ => "Failed to load post",
+                };
+            }
+
             return response;
         }
     }
--- Kickstarter.Core/Services/PostsService.cs
+++ Kickstarter.Core/Services/PostsService.cs
@@ -3,6 +3,7 @@ using Amazon.DynamoDBv2.Model;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,12 @@ namespace Kickstarter.Core.Services
                     Key = new Dictionary<string, AttributeValue>() { { "id", new AttributeValue { S = id } } },
                 };
                 var response = await client.GetItemAsync(request);
+                
+                if (response.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(response.HttpStatusCode.ToString());
+                }
+                
                 post = JsonConvert.SerializeObject(response.Item);
             }
             return post;
